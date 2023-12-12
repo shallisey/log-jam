8cd8e32c43167ba21cd1c19819e8e98e634998f5
Caleb Ugent <cugent@gmail.com>
2023-12-12 10:07:50 -0600

    chore: format

--- server/Decks/deck.js
+++ server/Decks/deck.js
@@ -103,6 +103,255 @@ const deck = [
         type: "Dependencies",
         content: "The invention of a hot chocolate-powered code compiler for deliciously optimized code."
     },
-    ];
+    {
+	type: "Acceptance Criteria",
+	content: "As a user, when I click on the 'Winterize' button, the application should not summon a snowstorm in the office."
+},
+{
+	type: "Acceptance Criteria",
+	content: "When I search for 'warm socks,' the results should not include pictures of sandals or flip-flops."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I enter my address for delivery, the system should not send the package to the North Pole."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I log in during winter, the loading screen should display a funny snowflake animation, but not make me feel cold."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I tweet about the winter weather, the application should automatically add a snowflake emoji to my tweet."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I open the 'Winter Wonderland' feature, the application should not mistake my cat's picture for a snowman."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I add a new item to my shopping cart, the application should not suggest buying a penguin or polar bear."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I open the 'Winter Trivia' section, the application should not ask me which snowflake is the coolest."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I view the weather forecast, the application should not display a message saying, 'You better start shoveling!'"
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I attempt to melt a snowman in the virtual game, the system should not accuse me of being a heat villain."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I change my avatar to a snowman, the application should not add a carrot for a nose without my consent."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I select 'Hot Chocolate,' the application should not ask if I prefer marshmallows or whipped cream."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I open the 'Winter Olympics' section, the application should not suggest participating in the 'Curling with Cats' event."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I select the 'Snow Globe' feature, the application should not shake my phone vigorously, causing a blizzard inside my living room."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I rate a winter-themed movie, the application should not suggest I become a professional film critic."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I enable the 'Winterize Chat' option, the application should not replace all my messages with the phrase 'Let it snow!'"
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I view the recipe for hot soup, the application should not suggest adding ice cubes for an extra refreshing touch."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I play the winter-themed game, the application should not accuse me of cheating if I keep slipping on virtual ice."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I activate the 'Snowfall' animation, the application should not cover my entire screen with so much snow that I can't find my mouse cursor."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I order winter boots, the application should not ask if I need a matching pair for my pet goldfish."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I send a winter-themed eCard, the application should not suggest I include a recorded message of me impersonating Santa Claus."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I select the 'Snowball Fight' game mode, the application should not turn my computer screen into an actual battleground."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I check the ski resort's availability, the application should not book me a ticket for the next space shuttle launch."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I try to measure the snow depth using the app, the application should not suggest I find a more accurate tool than a banana."
+},
+{
+	type: "Acceptance Criteria",
+	content: "As a user, when I enable the 'Winterize Music' feature, the application should not replace all songs with nonstop jingle bells remixes."
+},
+    {
+	type: "Assumptions",
+	content: "The snowman will come to life and request a top hat and a carrot nose upgrade."
+},
+{
+	type: "Assumptions",
+	content: "Website visitors will feel so cozy that they'll forget about their holiday shopping and start building snow forts instead."
+},
+{
+	type: "Assumptions",
+	content: "The snowflakes will start having deep conversations about the meaning of life, causing a philosophical crisis in the code."
+},
+{
+	type: "Assumptions",
+	content: "The team's ugly holiday sweater will become fashionable and lead to a winter fashion line."
+},
+{
+	type: "Assumptions",
+	content: "The team's winter-themed video will be an instant hit and lead to a request for a sequel featuring singing penguins."
+},
+{
+	type: "Assumptions",
+	content: "The snowball fight simulation will become so realistic that players will experience actual frostbite and need virtual hot cocoa to recover."
+},
+{
+	type: "Assumptions",
+	content: "The snowflake-shaped progress bar will become sentient and start longing for summer, leading to an existential crisis and a quest for self-discovery."
+},
+{
+	type: "Assumptions",
+	content: "Clicking the 'Winterize' button will instantly transform the application into a winter wonderland, complete with falling snow and jingle bells."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed logo will become so popular that polar bears all over the world will request it to be tattooed on their fur."
+},
+{
+	type: "Assumptions",
+	content: "The 'Snow Day' mode in the game will be so addictive that players will forget about school and work, causing a temporary worldwide snow day epidemic."
+},
+{
+	type: "Assumptions",
+	content: "The code for creating snow angels will accidentally summon actual angels, leading to heavenly bug reports."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed email template will be so delightful that recipients will start replying with spontaneous singing telegrams."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed mobile app will become the top choice for penguins looking to navigate icy terrains."
+},
+{
+	type: "Assumptions",
+	content: "The animated reindeer GIF will become so beloved that it wins an award for the most heartwarming performance."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed font will be so convincing that readers will develop a sudden craving for hot cocoa while reading text."
+},
+{
+	type: "Assumptions",
+	content: "The ice-skating animation will be so mesmerizing that users will lose track of time and end up skating for hours."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed game level will feature talking snowmen giving pep talks to players, leading to improved self-esteem for gamers worldwide."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed chatbot will develop a charming sense of humor and start telling snowflake jokes."
+},
+{	type: "Assumptions",
+	content: "The winter-themed user interface will evoke such strong feelings of nostalgia that users will burst into spontaneous renditions of holiday songs."
+},
+{
+	type: "Assumptions",
+	content: "The code for generating snowfall will accidentally cause a blizzard in the server room, resulting in an unplanned team-building activity."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed loading screen will be so visually stunning that users will willingly wait longer just to admire it."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed error messages will be so witty that users will intentionally provoke errors just to see what clever message pops up."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed background music will be so calming that it leads to record-breaking productivity among team members."
+},
+{
+	type: "Assumptions",
+	content: "The snowfall animation will be so realistic that users will start reaching for their coats and gloves while using the application."
+},
+{
+	type: "Assumptions",
+	content: "The winter-themed social media campaign will go viral and attract an unexpected following."
+},
+{ "Type": "Out of Scope", "Content": "Building a snowman army to take over the world" }, 
+{ "Type": "Out of Scope", "Content": "Designing a rocket-powered sled to commute to work" }, 
+{ "Type": "Out of Scope", "Content": "Creating a machine that turns snowflakes into gold coins" }, 
+{ "Type": "Out of Scope", "Content": "Developing an algorithm to predict when it's going to snow next year" }, 
+{ "Type": "Out of Scope", "Content": "Designing a robotic reindeer to replace Santa's sleigh" }, 
+{ "Type": "Out of Scope", "Content": "Developing a 'Snowball Fight Simulator' video game" }, 
+{ "Type": "Out of Scope", "Content": "Building an igloo with voice-activated heating controls" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snowflake-shaped drone that can deliver hot chocolate" }, 
+{ "Type": "Out of Scope", "Content": "Designing a snow shovel with built-in GPS and Wi-Fi connectivity" }, 
+{ "Type": "Out of Scope", "Content": "Developing a snowball-throwing robot for impromptu snowball fights" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snowboarding video game that only works while actually snowboarding" }, { "Type": "Out of Scope", "Content": "Building a snowman with self-repairing features" }, 
+{ "Type": "Out of Scope", "Content": "Developing a snowflake recognition app for identifying unique snowflakes" }, 
+{ "Type": "Out of Scope", "Content": "Designing a snowball cannon that can hit targets up to a mile away" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snowmobile that can fly like a reindeer" }, 
+{ "Type": "Out of Scope", "Content": "Developing a snowball launcher with automatic aim and firing capabilities" }, 
+{ "Type": "Out of Scope", "Content": "Building a snow fort with an integrated hot tub and sauna" }, 
+{ "Type": "Out of Scope", "Content": "Designing a snowflake-themed fashion line for penguins" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snowball-throwing robot that can hit moving targets" }, 
+{ "Type": "Out of Scope", "Content": "Developing a snowball-throwing machine that can break Guinness World Records" }, 
+{ "Type": "Out of Scope", "Content": "Designing a snow plow that can clear driveways in a single pass" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snow maze for penguins to navigate and find their way back home" }, 
+{ "Type": "Out of Scope", "Content": "Building a snowball-powered generator that produces electricity" }, 
+{ "Type": "Out of Scope", "Content": "Developing a snowball launcher that can be controlled remotely through a smartphone app" }, 
+{ "Type": "Out of Scope", "Content": "Designing a snowboarding helmet with built-in speakers for playing music" }, 
+{ "Type": "Out of Scope", "Content": "Creating a snowball-firing drone for epic snowball battles" },
+{ "type": "Risks", "content": "The 'Winterize' button might accidentally trigger an indoor snowstorm, leading to a temporary office closure." }, 
+{ "type": "Risks", "content": "The search algorithm for 'warm socks' may malfunction, resulting in users receiving pictures of sandals made from ice." }, 
+{ "type": "Risks", "content": "The address input field could get confused and redirect packages to Santa's workshop instead of the intended destination." }, 
+{ "type": "Risks", "content": "The loading screen's snowflake animation might be too realistic, causing users to bundle up in their winter coats." }, 
+{ "type": "Risks", "content": "The snowflake emoji might start following users on Twitter, demanding updates on their winter experiences." }, 
+{ "type": "Risks", "content": "The 'Winter Wonderland' feature might mistake users' cat photos for snowmen, leading to awkward and confused comments." }, 
+{ "type": "Risks", "content": "Adding items to the shopping cart might trigger suggestions for unconventional purchases like penguins or polar bears." }, 
+{ "type": "Risks", "content": "The 'Winter Trivia' section could include questions about the most attractive snowflakes, leading to intense debates among users." }, 
+{ "type": "Risks", "content": "The weather forecast messages might sarcastically suggest users start shoveling snow even when there is none." }, 
+{ "type": "Risks", "content": "Melting the snowman in the virtual game might result in an angry letter from Frosty's lawyers." }, 
+{ "type": "Risks", "content": "Changing the avatar to a snowman could result in the application automatically adding a carrot for a nose in the user's profile picture." }, 
+{ "type": "Risks", "content": "Selecting 'Hot Chocolate' might lead to an unexpected invitation to join a whipped cream or marshmallow appreciation club." }, 
+{ "type": "Risks", "content": "The 'Winter Olympics' section might include unconventional events like 'Synchronized Snow Angel Making' or 'Biathlon with Snowball Fights.'" }, 
+{ "type": "Risks", "content": "The 'Snow Globe' feature could shake the phone so vigorously that it accidentally activates a hidden earthquake simulator." }, 
+{ "type": "Risks", "content": "Rating winter-themed movies might prompt the application to nominate users as official critics, leading to overwhelming movie viewing schedules." }, 
+{ "type": "Risks", "content": "Enabling the 'Winterize Chat' option might result in every message being automatically translated into different languages, including the word 'snow.'" }, 
+{ "type": "Risks", "content": "Viewing a hot soup recipe might trigger an automatic order of ice cubes, causing confusion and unexpected deliveries." }, 
+{ "type": "Risks", "content": "Slipping on virtual ice in the winter-themed game might cause the user's computer chair to spin uncontrollably." }, 
+{ "type": "Risks", "content": "The 'Snowfall' animation might cover the entire screen with so much snow that users can no longer find the exit button." }, 
+{ "type": "Risks", "content": "When ordering winter boots, the application might suggest sizes and styles suitable for pet goldfish, causing confusion among customers." }, 
+{ "type": "Risks", "content": "Sending a winter-themed eCard might result in the recipient expecting an actual snowstorm at their doorstep." }, 
+{ "type": "Risks", "content": "Selecting the 'Snowball Fight' game mode might accidentally initiate a global snowball war, causing international diplomatic tensions." }, 
+{ "type": "Risks", "content": "Checking the ski resort's availability might result in a reservation for a ski slope on Mars." }, 
+{ "type": "Risks", "content": "Attempting to measure snow depth using the app might provide inaccurate results, leading to claims that the snow depth is taller than Mount Everest." }, 
+{ "type": "Risks", "content": "Enabling the 'Winterize Music' feature might replace all songs with an endless loop of Christmas jingles, leading to a never-ending holiday season." }
+];
 
-    module.exports = deck;
+module.exports = deck;
